b11c6cf4eb66d7ac4749e138bc0bdefac9bcca44
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2025-09-02 02:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.33%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> visited;
-        //if n does not exist in visited
-        while(visited.find(n)==visited.end()){
-            visited.insert(n);
-            n=sumOfSquare(n);
-            if(n==1) return true;
-        }
-        return false;
+       int slow=n;
+       int fast=sumOfSquare(n);
+       while(slow!=fast){
+        fast=sumOfSquare(fast);
+        fast=sumOfSquare(fast);
+        slow=sumOfSquare(slow);
+       } 
+       return fast==1;
     }
     int sumOfSquare(int n){
         int output=0;
-        while(n>0){
-            int digit=n%10;
-            digit=digit*digit;
-            output+=digit;
-            n=n/10;
+        while(n!=0){
+            output+=(n%10)*(n%10);
+            n/=10;
         }
         return output;
     }
