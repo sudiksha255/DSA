99a5245f07a36bb430d754a7cf3ae5350a854268
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2025-11-02 03:38:54 +0530

    Time: 82 ms (80.10%) | Memory: 114.5 MB (95.77%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -4,24 +4,23 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<pair<int,int>>qs;
         qs.reserve(queries.size());
-        for(int i=0;i<(int)queries.size();i++){
+        for(int i=0;i<queries.size();i++){
             qs.push_back({queries[i],i});
         }
         sort(qs.begin(),qs.end());
-        
-        vector<int>ans(queries.size(),-1);
+        vector<int> ans(queries.size(),-1);
         using P=pair<int,int>;
-        priority_queue<P,vector<P>,greater<P>> pq;
+        priority_queue<P,vector<P>,greater<P>>minHeap;
         int i=0;
-        for(auto[q,idx]:qs ){
-            while(i<(int)intervals.size()&&intervals[i][0]<=q){
-                int L=intervals[i][0],R=intervals[i][1];
-                pq.push({R-L+1,R});
-                i++;
-            }
-            while(!pq.empty()&&pq.top().second<q) pq.pop();
-
-            if(!pq.empty()) ans[idx]=pq.top().first;
+        for(auto[q,idx]:qs){
+        while(i<intervals.size()&&intervals[i][0]<=q){
+            int L=intervals[i][0],R=intervals[i][1];
+            minHeap.push({R-L+1,R});
+            i++;
+        }
+        while(!minHeap.empty()&&minHeap.top().second<q) minHeap.pop();
+        
+        if(!minHeap.empty()) ans[idx]=minHeap.top().first;
         }
         return ans;
     }
