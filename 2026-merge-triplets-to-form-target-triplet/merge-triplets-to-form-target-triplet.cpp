af5cf25cdc7352d827fde339b1156e66de1bf103
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2025-12-02 18:32:48 +0530

    Time: 0 ms (100.00%) | Memory: 150.6 MB (66.96%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -6,7 +6,8 @@ public:
         x|=t[0]==target[0]&&t[1]<=target[1]&&t[2]<=target[2];
         y|=t[0]<=target[0]&&t[1]==target[1]&&t[2]<=target[2];
         z|=t[0]<=target[0]&&t[1]<=target[1]&&t[2]==target[2];
+        if(x&&y&&z) return true;
         }
-        return (x&&y&&z)?true:false;
+        return false;
     }
 };
