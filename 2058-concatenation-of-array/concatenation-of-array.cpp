80cf04d1c7bc39e91d4c8edd388312f5e199c55c
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2026-01-19 04:52:12 +0530

    Time: 3 ms (10.38%) | Memory: 16.7 MB (94.36%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -4,9 +4,7 @@ public:
         int n=nums.size();
         vector<int> ans(2*n);
         for(int i=0;i<n;i++){
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
-            
+            ans[i]=ans[i+n]=nums[i];
         }
         return ans;
     }
