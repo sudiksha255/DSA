d79af0b54ffdb209fc1822f0a4df56491b659213
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2026-05-01 03:21:12 +0530

    Time: 223 ms (63.25%) | Memory: 10.8 MB (52.25%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,31 +1,28 @@
 class Solution {
-
-
 public:
-int rows,columns;
+int rows,cols;
+vector<vector<bool>> visited;
+
     bool exist(vector<vector<char>>& board, string word) {
-        rows=board.size(),columns=board[0].size();
+        rows=board.size();
+        cols=board[0].size();
+        visited=vector<vector<bool>>(rows,vector<bool>(cols,false));
         for(int r=0;r<rows;r++){
-            for(int c=0;c<columns;c++){
-                if(dfs(board,word,r,c,0)) return true;
+            for(int c=0;c<cols;c++){
+                if(dfs(board,word,r,c,0)){
+                    return true;
+                }
             }
         }
-    
-    return false;
+        return false;
     }
-    bool dfs(vector<vector<char>>&board,string word,int r,int c,int i){
-    if(i==word.size()) return true;
-    if(r<0||c<0||r>=rows||c>=columns||board[r][c]=='#'||board[r][c]!=word[i]) return false;
+    bool dfs(vector<vector<char>>&board,string &word,int r,int c,int i ){
+        if(i==word.size()) return true;
+        if(r<0||c<0||r>=rows||c>=cols||board[r][c]!=word[i]||visited[r][c]) return false;
+        visited[r][c]=true;
+        bool res=dfs(board,word,r+1,c,i+1)||dfs(board,word,r-1,c,i+1)||dfs(board,word,r,c+1,i+1)||dfs(board,word,r,c-1,i+1);
+        visited[r][c]=false;
 
-    board[r][c]='#';
-    bool res=dfs(board,word,r+1,c,i+1)||
-             dfs(board,word,r-1,c,i+1)||
-             dfs(board,word,r,c+1,i+1)||
-             dfs(board,word,r,c-1,i+1);
-    board[r][c]=word[i];
-    return res;
-    
- 
-}
-
+        return res;
+    }
 };
