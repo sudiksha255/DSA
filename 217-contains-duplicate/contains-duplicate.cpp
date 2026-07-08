662cd72fe50d3be2df54a9069356029338399a51
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2026-07-08 19:55:22 +0530

    Time: 75 ms (18.53%) | Memory: 111.4 MB (8.88%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-// using unordered map(Hash Map) , TC->O(n) 
     bool containsDuplicate(vector<int>& nums) {
-   sort(nums.begin(),nums.end());
-        int i=0;
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i++]==nums[j]){
-                    return true;
-                }
-            }
-        
-        return false;
+       unordered_set<int> seen;
+       for(int num:nums){
+        if(seen.count(num)){
+            return true;
+        }
+        seen.insert(num);
+       } 
+       return false;
     }
 };
