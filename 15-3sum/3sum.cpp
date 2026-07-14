b480fbd933d9456951b8acf9dcd73f459216b3cf
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2026-07-15 02:43:43 +0530

    Time: 36 ms (97.53%) | Memory: 29.1 MB (47.65%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-          vector<vector<int>> ans;
-           sort(nums.begin(),nums.end());
-          for(int i=0;i<nums.size();i++){
-            //if the first no. is greater than 0 then no possibility that the sum of three digits would be 0 since array is sorted and all digits would be grater 
-            if(nums[i]>0) break;
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            int left=i+1,right=nums.size()-1;
-            while(left<right){
-                int sum=nums[left]+nums[right]+nums[i];
-                if(sum>0) right--;
-                else if(sum<0) left++;
-                else {
-                    ans.push_back({nums[i],nums[left],nums[right]});
-                    left++;
-                    right--;
-                    while(left<right&&nums[left]==nums[left-1]) left++;
-                    while(left<right&&nums[right]==nums[right+1]) right--;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        for(int i=0;i<nums.size();i++){
 
-                }
+            if(nums[i]>0) break;
+
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
             }
-            
-          }return ans;
+
+            int l=i+1,r=nums.size()-1;
+            while(l<r){
+                int sum=nums[l]+nums[r]+nums[i];
+                if(sum>0) r--;
+                else if(sum<0) l++;
+                else {
+                    result.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                
+
+                while(l<r&&nums[l]==nums[l-1]) l++;
+                while(l<r&&nums[r]==nums[r+1]) r--;
+                }
+
+            }
+
+
+        }
+        return result;
     }
 };
