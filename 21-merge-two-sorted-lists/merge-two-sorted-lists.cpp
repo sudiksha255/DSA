ad4e8656bc0fef504402dae2409a59837a332f40
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2026-07-17 01:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.90%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -17,13 +17,14 @@ public:
             if(list1->val<=list2->val){
                 node->next=list1;
                 list1=list1->next;
-                node=node->next;
+                
             }
             else{
                 node->next=list2;
                 list2=list2->next;
-                node=node->next;
+              
             }
+              node=node->next;
         }
         if(list1){
             node->next=list1;
