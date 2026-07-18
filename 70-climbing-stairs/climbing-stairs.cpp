ce6d373028ab98c8bd22c7f5d64f10da2cadf88f
sudiksha255 <137460518+sudiksha255@users.noreply.github.com>
2026-07-18 21:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (58.15%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        double sqrt5=sqrt(5);
+        double phi=(1+sqrt5)/2;
+        double psi=(1-sqrt5)/2;
+        n++;
+        return round((pow(phi,n)-pow(psi,n))/sqrt5);
+    }
+};
